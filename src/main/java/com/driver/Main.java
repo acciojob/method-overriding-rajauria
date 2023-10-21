d0556c926c8c9c67c1acb06b1a538c5f0b435f37
Himanshu Rajauria <hrajauria1997@gmail.com>
2023-10-21 22:00:45 +0530

    Again changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,9 +17,12 @@ public class Main {
     public static void main(String[] args) {
         B objB = new B();
 
+        // Calling the method from class A
+        String resultFromA = objB.meth();
+        System.out.println( resultFromA);
 
-
+        // Calling the overridden method from class B
         String resultFromB = objB.meth();
-        System.out.println(resultFromB);
+        System.out.println ( resultFromB);
     }
 }
